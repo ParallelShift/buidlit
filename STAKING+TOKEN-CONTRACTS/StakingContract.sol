54d3eb6bb200b84895f805ead1f37d364ea88c9d
MacGuyver <89907172+macguyversmusic@users.noreply.github.com>
2022-03-13 04:36:45 -0400

    second commit
    ordered things
    put in time locking and stake time locking

--- STAKING+TOKEN-CONTRACTS/StakingContract.sol
+++ STAKING+TOKEN-CONTRACTS/StakingContract.sol
@@ -1,15 +1,14 @@
 pragma solidity ^0.8.6;
-// "SPDX-License-Identifier: UNLICENSED"
+// "SPDX-License-Identifier: MIT"
 import "./BuidlITToken.sol";
 /////////////////////////////
 contract TokenStaking is ReentrancyGuard{
     string public name = "Farming / Token dApp";
     BuidlITToken public BuidlITToken;
-
-//work out how to get the block time in to this lot to lock things up
-        uint public claimReady; //save claim  time
-        uint public cooldownTime = 0 days; // cooldown time
-////////////////////////////////////////////////////////////
+        uint public stakeLock; 
+        uint public stakeCooldownTime = 180 days; //stake locked time 6 months 180 days
+        uint public claimReady; 
+        uint public rewards_claim_frequency = 0 days; // cooldown time
 
     address public owner;
 
@@ -25,49 +24,31 @@ contract TokenStaking is ReentrancyGuard{
     constructor(BuidlITToken _BuidlITToken) payable {
         BuidlITToken = _BuidlITToken;
         owner = msg.sender;
-        claimReady = block.timestamp + cooldownTime;
-
+        claimReady = block.timestamp + rewards_claim_frequency;
     }
-
-    //stake tokens function
 
     function stakeTokens(uint256 _amount) public {
         require(_amount > 0, "amount cannot be 0");
-
-        //User adding tokens
+        stakeLock = block.timestamp + stakeCooldownTime;
         BuidlITToken.transferFrom(msg.sender, address(this), _amount);
         totalStaked = totalStaked + _amount;
-        //updating staking balance for user by mapping
         stakingBalance[msg.sender] = stakingBalance[msg.sender] + _amount;
-        //checking if user staked before or not, if NOT staked adding to array of stakers
-        if (!hasStaked[msg.sender]) {
-            stakers.push(msg.sender);
-        }
-        //updating staking status
+        if (!hasStaked[msg.sender]) 
+            {stakers.push(msg.sender);}
         hasStaked[msg.sender] = true;
         isStakingAtm[msg.sender] = true;
     }
 
     function unstakeTokens() public {
-        //get staking balance for user
-// require(!timelocked);
+        require(stakeLock <= block.timestamp, "You can't claim now.");
         uint256 balance = stakingBalance[msg.sender];
-
-        //amount should be more than 0
         require(balance > 0, "amount has to be more than 0");
-
-        //transfer staked tokens back to user
         BuidlITToken.transfer(msg.sender, balance);
         totalStaked = totalStaked - balance;
-
-        //reseting users staking balance
         stakingBalance[msg.sender] = 0;
-
-        //updating staking status
         isStakingAtm[msg.sender] = false;
     }
 
-    //claim tokens
     function claimRewards() public {
         require(msg.sender.isStakingAtm, "you are not staked");
         require(claimReady <= block.timestamp, "You can't claim now.");
@@ -76,21 +57,10 @@ contract TokenStaking is ReentrancyGuard{
         uint _withdrawableBalance = mulScale(stakingBalance[msg.sender], 100, 10000); 
         // 100/ 10000 basis points = 1%        
             if (_withdrawableBalance > 0) {
-                claimReady = block.timestamp + cooldownTime;
+                claimReady = block.timestamp + rewards_claim_frequency;
                 _withdrawableBalance = 0;
                 BuidlITToken.transfer(recipient, _withdrawableBalance);
             }
         }
     }
-
-    function claimTokens() public nonReentrant {
-    
-        if(token.balanceOf(address(this)) <= _withdrawableBalance) {
-            token.transfer(teamWallet, token.balanceOf(address(this)));
-        } else {
-
-            token.transfer(teamWallet, _withdrawableBalance); 
-        }
-    }
-
 }
